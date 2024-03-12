1ed0462e9ba68ea21c112490fa0cc58ec6c22f81
AddiSim <addisim12@gmail.com>
2024-03-12 14:59:40 +0000

    final3

--- src/main/java/is/hi/hbv202g/assignment8/LibrarySystem.java
+++ src/main/java/is/hi/hbv202g/assignment8/LibrarySystem.java
@@ -2,43 +2,49 @@ package is.hi.hbv202g.assignment8;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class LibrarySystem {
 
     private List<Book> books;
-    private Map<String, User> users;
+    private List<User> users;
     private List<Lending> lendings;
 
     public LibrarySystem() {
         books = new ArrayList<>();
-        users = new HashMap<>();
+        users = new ArrayList<>();
         lendings = new ArrayList<>();
     }
-    
+
     public void addBookWithTitleAndNameOfSingleAuthor(String title, String authorName) throws EmptyAuthorListException {
-        Author author = new Author(authorName);
-        List<Author> authors = new ArrayList<>();
-        authors.add(author);
-        Book book = new Book(title, authors);
-        books.add(book);
+        try {
+            Author author = new Author(authorName);
+            List<Author> authors = new ArrayList<>();
+            authors.add(author);
+            Book book = new Book(title, authors);
+            books.add(book);
+        } catch (EmptyAuthorListException e) {
+            e.printStackTrace();
+        }
     }
 
     public void addBookWithTitleAndAuthorList(String title, List<Author> authors) throws EmptyAuthorListException {
-        Book book = new Book(title, authors);
-        books.add(book);
+        try {
+            Book book = new Book(title, authors);
+            books.add(book);
+        } catch (EmptyAuthorListException e) {
+            e.printStackTrace();
+        }
     }
 
     public void addStudentUser(String name, boolean feePaid) {
         User student = new Student(name, feePaid);
-        users.put(name, student);
+        users.add(student);
     }
 
     public void addFacultyMemberUser(String name, String department) {
         User facultyMember = new FacultyMember(name, department);
-        users.put(name, facultyMember);
+        users.add(facultyMember);
     }
 
     public Book findBookByTitle(String title) throws UserOrBookDoesNotExistException {
@@ -51,11 +57,12 @@ public class LibrarySystem {
     }
 
     public User findUserByName(String name) throws UserOrBookDoesNotExistException {
-        User user = users.get(name);
-        if (user == null) {
-            throw new UserOrBookDoesNotExistException("User with name '" + name + "' does not exist.");
+        for (User user : users) {
+            if (user.getName().equals(name)) {
+                return user;
+            }
         }
-        return user;
+        throw new UserOrBookDoesNotExistException("User with name '" + name + "' does not exist.");
     }
 
     public void borrowBook(User user, Book book) {
@@ -72,21 +79,7 @@ public class LibrarySystem {
         }
     }
 
-    public void returnBook(User user, Book book) throws Exception {
-        boolean found = false;
-        for (Lending lending : lendings) {
-            if (lending.getBook().equals(book)) {
-                if (lending.getUser().equals(user)) {
-                    lendings.remove(lending);
-                    found = true;
-                    break;
-                } else {
-                    throw new Exception("Book was borrowed by a different user.");
-                }
-            }
-        }
-        if (!found) {
-            throw new Exception("Book was not borrowed.");
-        }
+    public void returnBook(User user, Book book) {
+        lendings.removeIf(lending -> lending.getBook().equals(book) && lending.getUser().equals(user));
     }
 }
